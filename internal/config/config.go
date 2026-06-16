a3f53bcbacde7c8647b2e35eff27cc9b4197d612
ShotaYoshida <dyqrks@gmail.com>
2026-06-16 15:42:13 +0900

    bump: v4.0.5 -> v4.0.6

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version is the application version
-var Version = "4.0.5"
+var Version = "4.0.6"
 
 type Config struct {
 	APISettings    APISettings    `json:"apiSettings"`
--- package.ps1
+++ package.ps1
@@ -1,4 +1,4 @@
-$Version     = "4.0.5"
+$Version     = "4.0.6"
 $PackageName = "BridgeGround_v$Version"
 
 Set-Location $PSScriptRoot
